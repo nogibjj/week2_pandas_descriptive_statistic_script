37607670536a1280b5fdd4bdb94375d2c03a9ac7
showaaaa <79520159+showaaaa@users.noreply.github.com>
2023-09-11 03:18:15 +0000

    fixfail

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def main():
     print("median of salary: " + str(df["Salary"].median()))
     plt.figure(figsize=(10, 6))
     plt.hist(df["Age"], bins=10, edgecolor="k", alpha=0.7)
-    plt.title(f"Distribution of Age")
+    plt.title("Distribution of Age")
     plt.xlabel("Age")
     plt.ylabel("Frequency")
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig(f"Age_distribution.png")
+    plt.savefig("Age_distribution.png")
     plt.show()
 
 
